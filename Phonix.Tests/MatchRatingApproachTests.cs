2f7bed2219749266b17848979df3c6721920c744
Shayne Boyer <spboyer@live.com>
2017-04-18 18:36:55 -0400

    add match rating tests

--- Phonix.Tests/MatchRatingApproachTests.cs
+++ Phonix.Tests/MatchRatingApproachTests.cs
@@ -1,18 +1,36 @@
-﻿using Xunit;
+﻿using System;
+using Xunit;
 
 namespace Phonix.Tests
 {
-    public class MatchRatingApproachTests
+  public class MatchRatingApproachTests
+  {
+    private static readonly string[] Words = new[] { "Spotify", "Spotfy", "Sputfy", "Sputfi" };
+
+    readonly MatchRatingApproach _generator = new MatchRatingApproach();
+
+
+    [Fact]
+    public void Should_Be_Similar()
     {
-        private static readonly string[] Words = new[] { "Spotify", "Spotfy", "Sputfy","Sputfi" };
-
-        readonly MatchRatingApproach _generator = new MatchRatingApproach();
-
-
-        [Fact]
-        public void Should_Be_Similar()
-        {
-            Assert.True(_generator.IsSimilar(Words));
-        }
+      Assert.True(_generator.IsSimilar(Words));
     }
+
+    [Fact]
+    public void Should_Match_Compute_6_Value()
+    {
+      var val = _generator.MatchRatingCompute("test", "TEST");
+      Console.WriteLine(val.ToString());
+      Assert.Equal(6, val);
+    }
+
+    [Fact]
+    public void Should_Match_Compute_0_Value()
+    {
+      var val = _generator.MatchRatingCompute("left", "right");
+      Console.WriteLine(val.ToString());
+      Assert.Equal(0, val);
+    }
+
+  }
 }
